cd71266946cb5949cec183f20a1a50b9e288567f
HoangNghia <dqhn2403@gmail.com>
2025-12-06 01:32:32 +0700

    setup project

---
+++ src/main/java/com/vn/mxh/MxhApplication.java
@@ -0,0 +1,13 @@
+package com.vn.mxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxhApplication.class, args);
+	}
+
+}
