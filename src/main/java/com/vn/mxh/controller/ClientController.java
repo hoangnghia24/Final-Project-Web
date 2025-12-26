bc5856a0d674d2abf8fea6f0f68d06418c4f03ae
HoangNghia <dqhn2403@gmail.com>
2025-12-27 00:49:05 +0700

    Revert "initialization"
    This reverts commit 9fd9aec003573e085cb8403d767e8a90f2e7cc13.

--- src/main/java/com/vn/mxh/controller/ClientController.java
+++ src/main/java/com/vn/mxh/controller/ClientController.java
@@ -1,51 +1,41 @@
 package com.vn.mxh.controller;
 
-import com.vn.mxh.domain.Post;
 import com.vn.mxh.domain.User;
-import com.vn.mxh.service.PostService;
 import com.vn.mxh.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
 public class ClientController {
-
     @Autowired
     private UserService userService;
+    // Tạm thời bỏ qua Repository để test giao diện trước
+    // private final UserRepository userRepository;
 
-    @Autowired
-    private PostService postService;
+    @GetMapping("/profile")
+    public String myProfile(Model model) {
+
+        // Trả về file: src/main/webapp/WEB-INF/view/client/profile.html
+        return "client/Profile";
+    }
+
+    // Tên hàm phải khớp với tên Query trong schema.graphqls: getUserByUsername
+    @QueryMapping
+    public User getUserByUsername(@Argument String username) {
+        // Tìm user trong DB bằng JPA Repository
+        // Giả sử Repository có hàm findByUsername
+        User user = this.userService.getUserByUsername(username);
+
+        return user; // Trả về null nếu không tìm thấy
+    }
 
     @GetMapping("/home")
     public String getHome() {
         return "client/Home";
-    }
-
-    @GetMapping("/profile")
-    public String myProfile(Model model) {
-        return "client/Profile";
-    }
-
-    @QueryMapping
-    public User getUserByUsername(@Argument String username) {
-        return this.userService.getUserByUsername(username);
-    }
-
-    @QueryMapping
-    public List<Post> getAllPublicPosts() {
-        return postService.getAllPosts();
-    }
-
-    @MutationMapping
-    public Post createPost(@Argument String content, @Argument String username) {
-        return postService.createPost(content, username);
     }
 }
